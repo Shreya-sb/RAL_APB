f50b417792db642528eb47003be513bd3d79c767
Shreya-sb <159764169+Shreya-sb@users.noreply.github.com>
2025-07-23 10:26:47 +0530

    updated

--- reg_seq.sv
+++ reg_seq.sv
@@ -61,8 +61,37 @@ class reg_seq extends uvm_sequence;
     uvm_status_e status;
     bit [7:0] rdata, rdata_m, dout;
 
-    ///////////////// REG2 /////////////////////
-    `uvm_info("SEQ", "====== REG2 ======", UVM_NONE);
+    ///////////////// REG1/////////////////////
+    `uvm_info("SEQ", "====== REG1 ======", UVM_NONE);
+
+    rdata   = regmodel.reg1_inst.get();
+    rdata_m = regmodel.reg1_inst.get_mirrored_value();
+    `uvm_info("SEQ", $sformatf("Initial REG1 -> Desired: %0h, Mirrored: %0h", rdata, rdata_m), UVM_NONE);
+
+    regmodel.reg1_inst.set(8'h01);
+    rdata   = regmodel.reg1_inst.get();
+    rdata_m = regmodel.reg1_inst.get_mirrored_value();
+    `uvm_info("SEQ", $sformatf("After set REG1 -> Desired: %0h, Mirrored: %0h", rdata, rdata_m), UVM_NONE);
+
+    regmodel.reg1_inst.update(status);
+    rdata   = regmodel.reg1_inst.get();
+    rdata_m = regmodel.reg1_inst.get_mirrored_value();
+    `uvm_info("SEQ", $sformatf("After update  REG1 -> Desired: %0h, Mirrored: %0h, Read: %0h", rdata, rdata_m, dout), UVM_NONE);
+
+
+
+    regmodel.reg1_inst.write(status,8'h01);
+    rdata   = regmodel.reg1_inst.get();
+    rdata_m = regmodel.reg1_inst.get_mirrored_value();
+    `uvm_info("SEQ", $sformatf("After write REG1 -> Desired: %0h, Mirrored: %0h, Read: %0h", rdata, rdata_m,dout), UVM_NONE);
+    regmodel.reg1_inst.read(status,dout);
+    rdata   = regmodel.reg1_inst.get();
+    rdata_m = regmodel.reg1_inst.get_mirrored_value();
+    `uvm_info("SEQ", $sformatf("After read REG1 -> Desired: %0h, Mirrored: %0h, Read: %0h", rdata, rdata_m,dout), UVM_NONE);
+
+
+ //////////////////////////REG2//////////////////////////////////////////////////////
+   `uvm_info("SEQ", "====== REG2 ======", UVM_NONE);
 
     rdata   = regmodel.reg2_inst.get();
     rdata_m = regmodel.reg2_inst.get_mirrored_value();
@@ -90,7 +119,7 @@ class reg_seq extends uvm_sequence;
     `uvm_info("SEQ", $sformatf("After read REG2 -> Desired: %0h, Mirrored: %0h, Read: %0h", rdata, rdata_m,dout), UVM_NONE);
 
     ///////////////// REG3 /////////////////////
-   /* `uvm_info("SEQ", "====== REG3 ======", UVM_NONE);
+    `uvm_info("SEQ", "====== REG3 ======", UVM_NONE);
 
     rdata   = regmodel.reg3_inst.get();
     rdata_m = regmodel.reg3_inst.get_mirrored_value();
@@ -102,10 +131,20 @@ class reg_seq extends uvm_sequence;
     `uvm_info("SEQ", $sformatf("After set REG3 -> Desired: %0h, Mirrored: %0h", rdata, rdata_m), UVM_NONE);
 
     regmodel.reg3_inst.update(status);
-    regmodel.reg3_inst.read(status, dout);
     rdata   = regmodel.reg3_inst.get();
     rdata_m = regmodel.reg3_inst.get_mirrored_value();
-    `uvm_info("SEQ", $sformatf("After update/read REG3 -> Desired: %0h, Mirrored: %0h, Read: %0h", rdata, rdata_m, dout), UVM_NONE);
+    `uvm_info("SEQ", $sformatf("After update REG3 -> Desired: %0h, Mirrored: %0h, Read: %0h", rdata, rdata_m, dout), UVM_NONE);
+    
+    regmodel.reg3_inst.write(status,8'h12);
+    rdata   = regmodel.reg3_inst.get();
+    rdata_m = regmodel.reg3_inst.get_mirrored_value();
+    `uvm_info("SEQ", $sformatf("After write REG3 -> Desired: %0h, Mirrored: %0h, Read: %0h", rdata, rdata_m,dout), UVM_NONE);
+    regmodel.reg3_inst.read(status,dout);
+    rdata   = regmodel.reg3_inst.get();
+    rdata_m = regmodel.reg3_inst.get_mirrored_value();
+    `uvm_info("SEQ", $sformatf("After read REG3 -> Desired: %0h, Mirrored: %0h, Read: %0h", rdata, rdata_m,dout), UVM_NONE);
+
+
 
     ///////////////// REG4 /////////////////////
     `uvm_info("SEQ", "====== REG4 ======", UVM_NONE);
@@ -120,19 +159,21 @@ class reg_seq extends uvm_sequence;
     `uvm_info("SEQ", $sformatf("After set REG4 -> Desired: %0h, Mirrored: %0h", rdata, rdata_m), UVM_NONE);
 
     regmodel.reg4_inst.update(status);
-    regmodel.reg4_inst.read(status, dout);
     rdata   = regmodel.reg4_inst.get();
     rdata_m = regmodel.reg4_inst.get_mirrored_value();
-    `uvm_info("SEQ", $sformatf("After update/read REG4 -> Desired: %0h, Mirrored: %0h, Read: %0h", rdata, rdata_m, dout), UVM_NONE);
+    `uvm_info("SEQ", $sformatf("After update REG4 -> Desired: %0h, Mirrored: %0h, Read: %0h", rdata, rdata_m, dout), UVM_NONE);
+     
+    regmodel.reg4_inst.write(status,8'h23);
+    rdata   = regmodel.reg4_inst.get();
+    rdata_m = regmodel.reg4_inst.get_mirrored_value();
+    `uvm_info("SEQ", $sformatf("After write REG4 -> Desired: %0h, Mirrored: %0h, Read: %0h", rdata, rdata_m,dout), UVM_NONE);
+    regmodel.reg4_inst.read(status,dout);
+    rdata   = regmodel.reg4_inst.get();
+    rdata_m = regmodel.reg4_inst.get_mirrored_value();
+    `uvm_info("SEQ", $sformatf("After read REG4 -> Desired: %0h, Mirrored: %0h, Read: %0h", rdata, rdata_m,dout), UVM_NONE);
 
-    ///////////////// REG1 (Read-Only) /////////////////////
-    `uvm_info("SEQ", "====== REG1 (Read-Only) ======", UVM_NONE);
 
-    regmodel.reg1_inst.read(status, dout);
-    rdata   = regmodel.reg1_inst.get();
-    rdata_m = regmodel.reg1_inst.get_mirrored_value();
-    `uvm_info("SEQ", $sformatf("REG1 Read-Only -> Desired: %0h, Mirrored: %0h, Read: %0h", rdata, rdata_m, dout), UVM_NONE);
-*/
+  
   endtask
 endclass
 
