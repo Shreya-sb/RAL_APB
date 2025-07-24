9327c1568caa41b78ffd811c3d0168d7e71f906c
Shreya S B <159764169+Shreya-sb@users.noreply.github.com>
2025-07-24 12:27:59 +0530

    Update seq_item.sv

--- seq_item.sv
+++ seq_item.sv
@@ -11,8 +11,4 @@ class seq_item extends uvm_sequence_item;
   function new(string name = "seq_item");
     super.new(name);
   endfunction
-
-  //constraint addr_range {paddr inside {[0:3]};}
-  //constraint psel_value {soft psel==1;}
- 
 endclass
