74b5c153349b8c7d203fd95a0372c96208e835ae
Shreya S B <159764169+Shreya-sb@users.noreply.github.com>
2025-08-05 16:29:14 +0530

    addes regression test

--- test.sv
+++ test.sv
@@ -73,3 +73,36 @@ class backdoor_test extends uvm_test;
     phase.phase_done.set_drain_time(this,20); 
   endtask 
 endclass
+
+class reg_test extends uvm_test;
+  `uvm_component_utils(reg_test)
+  reg_seq seq1;
+  reset_seq seq2;
+
+  environment env;
+
+  function new(string name = "reg_test",uvm_component parent = null);
+    super.new(name,parent);
+  endfunction
+
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    env = environment::type_id::create("env",this);
+    seq1 = reg_seq::type_id::create("seq1", this);
+    seq2 = reset_seq::type_id::create("seq2", this);
+  endfunction
+  virtual task run_phase(uvm_phase phase);
+     
+    phase.raise_objection(this);
+    seq1.regmodel = env.regmodel;
+    seq1.start(env.agent_inst.seqr);
+    phase.drop_objection(this);
+    phase.phase_done.set_drain_time(this, 20);
+
+    phase.raise_objection(this);
+    seq2.regmodel = env.regmodel;
+    seq2.start(env.agent_inst.seqr);
+    phase.drop_objection(this);
+    phase.phase_done.set_drain_time(this, 20);
+  endtask
+endclass
